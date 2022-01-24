694f7b57f2ed9803f1fce6e9f3ea925d9da734dc
LIM <lcg5476@navre.com>
2022-01-25 00:16:57 +0900

    채걸2

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'miniproject'
 
 urlpatterns = [
     path('maps/', views.maps, name = 'maps'),
+    path('login/',
+        auth_views.LoginView.as_view(
+            template_name='miniproject/login.html'
+        ),
+        name='login'
+    ),
 ]
