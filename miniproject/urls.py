c4d03a9e732b09364d458a3e362dd7803039d3f0
KimGyeongJun <KimGyeongJun@gmail.com>
2022-01-25 16:19:02 +0900

    login, signup 초안

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -5,4 +5,7 @@ app_name = 'miniproject'
 
 urlpatterns = [
     path('maps/', views.maps, name = 'maps'),
+    path('login/', views.login, name = 'login'),
+    path('logout/', views.logout, name = 'logout'),
+    path('signup/', views.signup, name = 'signup'),
 ]
