3a4a7aaf12a3a4a856d0ee95fcdf1cdd141f0b57
KimGyeongJun <KimGyeongJun@gmail.com>
2022-01-26 00:16:21 +0900

    commit

--- miniproject/views.py
+++ miniproject/views.py
@@ -1,54 +1,23 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib import auth
+from django.http import HttpResponse
 
 def maps(request):
     return render(request, 'miniproject/maps.html')
 
-# 회원 가입
-def signup(request):
-    # signup 으로 POST 요청이 왔을 때, 새로운 유저를 만드는 절차를 밟는다.
-    if request.method == 'POST':
-        # password와 confirm에 입력된 값이 같다면
-        if request.POST['password'] == request.POST['confirm']:
-            # user 객체를 새로 생성
-            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
-            # 로그인 한다
-            auth.login(request, user)
-            return redirect('/')
-    # signup으로 GET 요청이 왔을 때, 회원가입 화면을 띄워준다.
-    return render(request, 'miniproject/signup.html')
-
-# 로그인
 def login(request):
-    # login으로 POST 요청이 들어왔을 때, 로그인 절차를 밟는다.
     if request.method == 'POST':
-        # login.html에서 넘어온 username과 password를 각 변수에 저장한다.
-        username = request.POST['username']
-        password = request.POST['password']
-
-        # 해당 username과 password와 일치하는 user 객체를 가져온다.
-        user = auth.authenticate(request, username=username, password=password)
-        
-        # 해당 user 객체가 존재한다면
-        if user is not None:
-            # 로그인 한다
-            auth.login(request, user)
-            return redirect('/')
-        # 존재하지 않는다면
+        user_id = request.POST.get('user_id')
+        user_pw = request.POST.get('user_pw')
+        try:
+            m = miniproject.objects.get(user_id=user_id, user_pw=user_pw)
+        except miniproject.DoesNotExist as e:
+            return HttpResponse('아이디와 비밀번호를 확인해주세요.')
         else:
-            # 딕셔너리에 에러메세지를 전달하고 다시 login.html 화면으로 돌아간다.
-            return render(request, 'login.html', {'error' : 'username or password is incorrect.'})
-    # login으로 GET 요청이 들어왔을때, 로그인 화면을 띄워준다.
+            request.session['user_id'] = m.user_id
+            request.session['user_name'] = m.user_name
+
+        # 회원정보 조회 실패 시 예외 발생
+        return redirect('member:login')
     else:
-        return render(request, 'miniproject/login.html')
-
-# 로그 아웃
-def logout(request):
-    # logout으로 POST 요청이 들어왔을 때, 로그아웃 절차를 밟는다.
-    if request.method == 'POST':
-        auth.logout(request)
-        return redirect('/')
-
-    # logout으로 GET 요청이 들어왔을 때, 로그인 화면을 띄워준다.
-    return render(request, 'miniproject/login.html')
+        return render(request, 'member/login_custom.html')
