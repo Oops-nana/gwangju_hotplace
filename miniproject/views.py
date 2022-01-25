46663405064647336b8946c2f35bf05b4c67f587
gykang23 <gykang23@naver.com>
2022-01-25 10:53:14 +0900

    bootstrap

--- miniproject/views.py
+++ miniproject/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def maps(request):
     return render(request, 'miniproject/maps.html')
+
+def login(request):
+    return render(request, 'miniproject/login.html')
