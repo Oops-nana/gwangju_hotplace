8d2db436960b5d50f616a51d2bac3c005dbe333b
wonjae <dnjswo6253@naver.com>
2022-01-30 15:52:16 +0900

    로그인시에만 메인기능 이용가능

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -6,8 +6,6 @@ app_name = 'miniproject'
 
 urlpatterns = [
     path('maps/', views.maps, name='maps'),
-    path('session/', views.login_session, name='session'),
     path('logout/', views.logout, name='logout'),
-    path('index/', views.index, name='index'),
     path('comment/', views.postCommentAndStar, name='comment'),
 ]
--- miniproject/views.py
+++ miniproject/views.py
@@ -1,7 +1,7 @@
 from . import CommentForms
 from main.models import Comment
 from django.utils import timezone
-import django
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from django.contrib.auth import login
@@ -13,42 +13,42 @@ from member.models import Member
 
 
 def maps(request):
-    place = CommonPlace.objects.all()
-    museumdtl = museumDetail.objects.all()
-    
-    data = []
-    comment_data = []
-    user_data = []
-    museum_data = []
-    for i in place:
-        i = model_to_dict(i)
-        data.append(i)
-    for j in museumdtl:
-        j = model_to_dict(j)
-        museum_data.append(j)
-    return render(request, 'miniproject/maps.html', 
-                  {'data': data, 'museum_data': museum_data})
+    if request.session.get('id'):
+        place = CommonPlace.objects.all()
+        museumdtl = museumDetail.objects.all()
+
+        data = []
+        comment_data = []
+        user_data = []
+        museum_data = []
+        for i in place:
+            i = model_to_dict(i)
+            data.append(i)
+        for j in museumdtl:
+            j = model_to_dict(j)
+            museum_data.append(j)
+        return render(request, 'miniproject/maps.html',
+                      {'data': data, 'museum_data': museum_data})
+    else:
+        return redirect(reverse('member:login'))
 
 
-def login_session(request):
-    user_pk = request.session.get('id')
+# def login_session(request):
+#     user_pk = request.session.get('id')
 
-    if user_pk:
-        fuser = Member.objects.get(pk=user_pk)
-    return HttpResponse(fuser)
+#     if user_pk:
+#         fuser = Member.objects.get(pk=user_pk)
+#     return HttpResponse(fuser)
 
     # return render(request, 'member/login.html')
 
+
 def logout(request):
-    del request.session['id'] # 개별 삭제
-    del request.session['user_name'] # 개별 삭제
-    request.session.flush() # 전체 삭제
+    del request.session['id']  # 개별 삭제
+    del request.session['user_name']  # 개별 삭제
+    request.session.flush()  # 전체 삭제
 
     return redirect('/member/login')
-
-
-def index(request):
-    return render(request, 'miniproject/index.html')
 
 
 def login_session(request):
